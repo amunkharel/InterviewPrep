59701cb5ea1efd2a305ae2f8f787068d4eea2eca
Amun Kharel <akharel@cs.unm.edu>
2020-06-07 06:34:16 -0600

    Longest Common Prefix withoud Divide and Conquer

--- src/General/Problem_3.java
+++ src/General/Problem_3.java
@@ -3,11 +3,23 @@ package General;
 public class Problem_3 {
 
     public static void main(String[] args) {
-        
+        String arr[] = {"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(arr));
     }
 
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0 || strs == null) return  "";
 
-        return "abc";
+        String prefix = strs[0];
+
+        for(int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
     }
 }
